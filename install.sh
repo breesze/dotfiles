ecc9a393cbbc237532b4d25195af643d07862408
breesze <1121154108@qq.com>
2020-05-02 20:55:56 +0800

    Use confirm instead of force option

--- install.sh
+++ install.sh
@@ -3,36 +3,36 @@
 # Make sure u r in the path where this script is 
 path=`pwd`
 # X
-ln -sfv $path/X/Xresources ~/.Xresources
-ln -sfv $path/X/xinitrc ~/.xinitrc
+ln -siv $path/X/Xresources ~/.Xresources
+ln -siv $path/X/xinitrc ~/.xinitrc
 # aria2
-ln -sfv $path/aria2/aria2.conf ~/.aria2/aria2.conf
-ln -sfv $path/aria2/delete.aria2.sh ~/.aria2/delete.aria2.sh
-ln -sfv $path/aria2/delete.sh ~/.aria2/delete.sh
-ln -sfv $path/aria2/dht.dat ~/.aria2/dht.dat
-ln -sfv $path/aria2/dht6.dat ~/.aria2/dht6.dat
+ln -siv $path/aria2/aria2.conf ~/.aria2/aria2.conf
+ln -siv $path/aria2/delete.aria2.sh ~/.aria2/delete.aria2.sh
+ln -siv $path/aria2/delete.sh ~/.aria2/delete.sh
+ln -siv $path/aria2/dht.dat ~/.aria2/dht.dat
+ln -siv $path/aria2/dht6.dat ~/.aria2/dht6.dat
 # fontconfig(DejavVu, Noto Sans CJK)
-ln -sfv $path/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
+ln -siv $path/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
 # i3-gaps, i3status
-ln -sfv $path/i3status/config ~/.config/i3status/config
-ln -sfv $path/i3status/net-speed.sh ~/.config/i3status/net-speed.sh
-ln -sfv $path/i3wm/config ~/.config/i3/config
+ln -siv $path/i3status/config ~/.config/i3status/config
+ln -siv $path/i3status/net-speed.sh ~/.config/i3status/net-speed.sh
+ln -siv $path/i3wm/config ~/.config/i3/config
 # mpd, ncmpcpp
-ln -sfv $path/mpd-ncmpcpp/bindings ~/.ncmpcpp/bindings
-ln -sfv $path/mpd-ncmpcpp/config ~/.ncmpcpp/config
-ln -sfv $path/mpd-ncmpcpp/mpd.conf ~/.config/mpd/mpd.conf
+ln -siv $path/mpd-ncmpcpp/bindings ~/.ncmpcpp/bindings
+ln -siv $path/mpd-ncmpcpp/config ~/.ncmpcpp/config
+ln -siv $path/mpd-ncmpcpp/mpd.conf ~/.config/mpd/mpd.conf
 # termite
-ln -sfv $path/termite/config ~/.config/termite/config
+ln -siv $path/termite/config ~/.config/termite/config
 # tmux
-ln -sfv $path/tmux/tmux.conf ~/.tmux.conf
+ln -siv $path/tmux/tmux.conf ~/.tmux.conf
 # vim
-ln -sfv $path/vim/keymap.vim ~/.vim/vim/keymap.vim
-ln -sfv $path/vim/plugin.vim ~/.vim/vim/plugin.vim
-ln -sfv $path/vim/vimrc ~/.vimrc
+ln -siv $path/vim/keymap.vim ~/.vim/vim/keymap.vim
+ln -siv $path/vim/plugin.vim ~/.vim/vim/plugin.vim
+ln -siv $path/vim/vimrc ~/.vimrc
 # zsh
-ln -sfv $path/zsh/zshrc ~/.zshrc
-ln -sfv $path/zsh/LESS_TERMCAP ~/.LESS_TERMCAP 
-ln -sfv $path/zsh/zpreztorc ~/.zpreztorc
-ln -sfv $path/zsh/lgfs.prompt.sh ~/.lgfs.prompt.sh
-ln -sfv $path/zsh/fish.prompt.sh ~/.fish.prompt.sh
-ln -sfv $path/zsh/alias.sh ~/.alias.sh
+ln -siv $path/zsh/zshrc ~/.zshrc
+ln -siv $path/zsh/LESS_TERMCAP ~/.LESS_TERMCAP 
+ln -siv $path/zsh/zpreztorc ~/.zpreztorc
+ln -siv $path/zsh/lgfs.prompt.sh ~/.lgfs.prompt.sh
+ln -siv $path/zsh/fish.prompt.sh ~/.fish.prompt.sh
+ln -siv $path/zsh/alias.sh ~/.alias.sh
