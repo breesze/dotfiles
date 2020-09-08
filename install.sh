421aed93a59701248079df943516369d2720db52
breezelw <1121154108@qq.com>
2020-09-08 14:30:52 +0800

    add keynav

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ ln -siv $path/X/Xresources ~/.Xresources
 ln -siv $path/X/xinitrc ~/.xinitrc
 ln -siv $path/X/redshift.conf ~/.config/redshift.conf
 ln -siv $path/X/rc.conf ~/.config/ranger/rc.conf
+ln -siv $path/X/keynavrc ~/.config/keynav/keynavrc
 # aria2
 # ln -siv $path/aria2/aria2.conf ~/.aria2/aria2.conf
 # ln -siv $path/aria2/delete.aria2.sh ~/.aria2/delete.aria2.sh
