daf146e72b9f409a4f57c4087cff2beda880de25
breezelw <1121154108@qq.com>
2020-10-07 01:54:10 +0800

    update config path

--- install.sh
+++ install.sh
@@ -1,52 +1,69 @@
 #! /bin/sh
 
-# Make sure u r in the path where this script is 
 path=`pwd`
-# X
-ln -siv $path/X/Xresources ~/.Xresources
-ln -siv $path/X/xinitrc ~/.xinitrc
-ln -siv $path/X/redshift.conf ~/.config/redshift.conf
-ln -siv $path/X/rc.conf ~/.config/ranger/rc.conf
-ln -siv $path/X/keynavrc ~/.config/keynav/keynavrc
-ln -siv $path/X/picom.conf ~/.config/picom/picom.conf
+
+# alacritty
+ln -siv $path/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
 # aria2
 # ln -siv $path/aria2/aria2.conf ~/.aria2/aria2.conf
 # ln -siv $path/aria2/delete.aria2.sh ~/.aria2/delete.aria2.sh
 # ln -siv $path/aria2/delete.sh ~/.aria2/delete.sh
 # ln -siv $path/aria2/dht.dat ~/.aria2/dht.dat
 # ln -siv $path/aria2/dht6.dat ~/.aria2/dht6.dat
-# fontconfig(DejavVu, Noto Sans CJK)
+
+# dwm
+ln -siv $path/dwm/startdwm.sh ~/.startdwm.sh
+
+# fontconfig
 ln -siv $path/fontconfig/fonts.conf ~/.config/fonts.conf
+
 # i3-gaps, i3status
 # ln -siv $path/i3status/config ~/.config/i3status/config
 # ln -siv $path/i3status/net-speed.sh ~/.config/i3status/net-speed.sh
 # ln -siv $path/i3wm/config ~/.config/i3/config
-# dwm
-# ln -siv $path/dwm/dwm-statusbar.sh ~/.dwm-statusbar.sh
-# ln -siv $path/dwm/fehbg ~/.fehbg
-ln -siv $path/dwm/startdwm.sh ~/.startdwm.sh
-# mpd, ncmpcpp
+
+# keynav
+ln -siv $path/keynav/keynavrc ~/.config/keynav/keynavrc
+
+# mpd ncmpcpp
 ln -siv $path/mpd-ncmpcpp/bindings ~/.config/ncmpcpp/bindings
 ln -siv $path/mpd-ncmpcpp/config ~/.config/ncmpcpp/config
 ln -siv $path/mpd-ncmpcpp/mpd.conf ~/.config/mpd/mpd.conf
+
+# picom(compton)
+ln -siv $path/picom/picom.conf ~/.config/picom/picom.conf
+
+# ranger
+ln -siv $path/ranger/rc.conf ~/.config/ranger/rc.conf
+
+# redshift
+ln -siv $path/redshift/redshift.conf ~/.config/redshift.conf
+
 # termite
-# ln -siv $path/termite/config ~/.config/termite/config
+ln -siv $path/termite/config ~/.config/termite/config
+
 # tmux
 ln -siv $path/tmux/tmux.conf ~/.tmux.conf
+
 # vim
-ln -siv $path/vim/vimrc ~/.vimrc
+ln -siv $path/vim/clang-format ~/.clang-format
 ln -siv $path/vim/coc-settings.json ~/.vim/coc-settings.json
 ln -siv $path/vim/keymap.vim ~/.vim/vim/keymap.vim
 ln -siv $path/vim/plugin.vim ~/.vim/vim/plugin.vim
-ln -siv $path/vim/clang-format ~/.clang-format
+ln -siv $path/vim/vimrc ~/.vimrc
+
+# xorg
+ln -siv $path/xorg/Xresources ~/.Xresources
+ln -siv $path/xorg/xinitrc ~/.xinitrc
+
+# zathura
+ln -siv $path/zathura/zathurarc ~/.config/zathura/zathurarc
+
 # zsh
-ln -siv $path/zsh/zshrc ~/.zshrc
 ln -siv $path/zsh/LESS_TERMCAP ~/.LESS_TERMCAP 
-ln -siv $path/zsh/zpreztorc ~/.zpreztorc
-ln -siv $path/zsh/lgfs.prompt.sh ~/.lgfs.prompt.sh
-ln -siv $path/zsh/fish.prompt.sh ~/.fish.prompt.sh
 ln -siv $path/zsh/alias.sh ~/.alias.sh
-# pacman
-# ln -siv $path/pacman/pacman.log /var/log/
-# alacritty
-ln -siv $path/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -siv $path/zsh/fish.prompt.sh ~/.fish.prompt.sh
+ln -siv $path/zsh/lgfs.prompt.sh ~/.lgfs.prompt.sh
+ln -siv $path/zsh/zpreztorc ~/.zpreztorc
+ln -siv $path/zsh/zshrc ~/.zshrc
