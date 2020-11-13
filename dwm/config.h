a839ec1c912aa3821de810a9291c336c361cbb72
breezelw <1121154108@qq.com>
2020-11-14 01:00:48 +0800

    modify gaps

--- dwm/config.h
+++ dwm/config.h
@@ -3,11 +3,12 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gaps      = 20;
+static const unsigned int gappih    = gaps;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = gaps;       /* vert inner gap between windows */
+static const unsigned int gappoh    = gaps;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = gaps;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"SarasaMonoCL:style=Semibold:size=10","FontAwesome5Free:style=Solid:size=9","FontAwesome5Free:style=Regular:size=9", "FontAwesome5Brands:style=Regular:size=9"};
