1cbc4fb5c8a81cd325e3165d71e23a306d712c8b
breezelw <1121154108@qq.com>
2020-11-14 20:04:36 +0800

    update config

--- dwm/config.h
+++ dwm/config.h
@@ -1,16 +1,16 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 2;  /* border pixel of windows */
-static const unsigned int snap     = 32; /* snap pixel */
+static const unsigned int borderpx = 2;         /* border pixel of windows */
+static const unsigned int snap     = 32;        /* snap pixel */
 static const unsigned int gaps     = 20;
-static const unsigned int gappih   = gaps; /* horiz inner gap between windows */
-static const unsigned int gappiv   = gaps; /* vert inner gap between windows */
-static const unsigned int gappoh   = gaps; /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov   = gaps; /* vert outer gap between windows and screen edge */
-static const int smartgaps         = 0;    /* 1 means no outer gap when there is only one window */
-static const int showbar           = 1;    /* 0 means no bar */
-static const int topbar            = 1;    /* 0 means bottom bar */
+static const unsigned int gappih   = gaps;      /* horiz inner gap between windows */
+static const unsigned int gappiv   = gaps;      /* vert inner gap between windows */
+static const unsigned int gappoh   = gaps;      /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov   = gaps;      /* vert outer gap between windows and screen edge */
+static const int smartgaps         = 0;         /* 1 means no outer gap when there is only one window */
+static const int showbar           = 1;         /* 0 means no bar */
+static const int topbar            = 1;         /* 0 means bottom bar */
 static const char *fonts[]         = {
     "SarasaMonoCL:style=Semibold:size=10", "FontAwesome5Free:style=Solid:size=9",
     "FontAwesome5Free:style=Regular:size=9", "FontAwesome5Brands:style=Regular:size=9"};
@@ -49,7 +49,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.49; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
