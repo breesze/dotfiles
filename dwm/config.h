df84f3f45a38559c5a490d57d092e30aa3ff7ec0
breezelw <1121154108@qq.com>
2020-11-14 22:19:57 +0800

    nothing

--- dwm/config.h
+++ dwm/config.h
@@ -31,6 +31,8 @@ static const char *colors[][3] = {
 
 /* tagging */
 static const char *tags[] = {"一", "二", "三", "四", "五", "六", "七", "八", "九"};
+// static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+// static const char *tags[] = { "Ⅰ", "Ⅱ", "Ⅲ", "Ⅳ", "Ⅴ", "Ⅵ", "Ⅶ", "Ⅷ", "Ⅸ" };
 
 static const Rule rules[] = {
 	/* xprop(1):
