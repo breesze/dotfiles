e1378892e81825f753e053f0cefc35f761742e27
breezelw <1121154108@qq.com>
2020-11-16 10:47:35 +0800

    change dmenu bg color

--- dwm/config.h
+++ dwm/config.h
@@ -80,7 +80,7 @@ static const Layout layouts[] = {
 #define XK_br XK_bracketright
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray, "-nf", col_white, "-sb", col_cyan, "-sf", col_gray, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_white, "-sb", col_cyan, "-sf", col_gray, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 #include <X11/XF86keysym.h>
 static Key keys[] = {
